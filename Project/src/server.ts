28948e0a790db3d3cb52dde2c0fe7673176b7a60
raffoliveira <rafaelollywer@gmail.com>
2021-02-23 16:31:16 -0300

    Second Day

--- Project/src/server.ts
+++ Project/src/server.ts
@@ -1,6 +1,13 @@
+import "reflect-metadata";
 import express, { request, response } from "express";
+import "./database";
+import { router } from "./routes";
 
 const app = express();
+app.use(express.json());
+app.use(router);
+
+
 
 /**
  * GET = busca
@@ -11,14 +18,14 @@ const app = express();
  */
 
 //http://localhost:3333/users
-app.get("/", (resquest, response) => {
-    // return response.send('Hello World!!');
-    return response.json({ 'message': 'Hello World' });
-})
+// app.get("/", (resquest, response) => {
+// return response.send('Hello World!!');
+//     return response.json({ 'message': 'Hello World' });
+// })
 
-app.post('/', (request, response) => {
-    return response.json({ 'message': 'The data is saved.' })
-})
+// app.post('/', (request, response) => {
+//     return response.json({ 'message': 'The data is saved.' })
+// })
 
 //listen(port, )
 app.listen(3333, () => console.log('Server is running!'));
