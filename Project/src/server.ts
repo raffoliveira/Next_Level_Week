2a973b91237326280d499544cb41481c189859f4
raffoliveira <rafaelollywer@gmail.com>
2021-02-24 15:57:43 -0300

    Third day - testing application

--- Project/src/server.ts
+++ Project/src/server.ts
@@ -1,32 +1,4 @@
-import "reflect-metadata";
-import express, { request, response } from "express";
-import "./database";
-import { router } from "./routes";
+import { app } from "./app";
 
-const app = express();
-app.use(express.json());
-app.use(router);
-
-
-
-/**
- * GET = busca
- * POST = salvar
- * PUT = alterar
- * DELETE = deletar
- * PATCH = alterar especificamente
- */
-
-//http://localhost:3333/users
-// app.get("/", (resquest, response) => {
-// return response.send('Hello World!!');
-//     return response.json({ 'message': 'Hello World' });
-// })
-
-// app.post('/', (request, response) => {
-//     return response.json({ 'message': 'The data is saved.' })
-// })
-
-//listen(port, )
 app.listen(3333, () => console.log('Server is running!'));
 
