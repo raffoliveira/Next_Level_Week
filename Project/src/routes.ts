d2c293e842abee301f59c0806cca559022fbdb5e
raffoliveira <rafaelollywer@gmail.com>
2021-02-26 15:47:54 -0300

    Fifth day - Validation

--- Project/src/routes.ts
+++ Project/src/routes.ts
@@ -1,4 +1,6 @@
 import { Router } from "express";
+import { AnswerController } from "./controllers/AnswerController";
+import { NpsController } from "./controllers/NpsController";
 import { SendMailController } from "./controllers/SendMailController";
 import { SurveysController } from "./controllers/SurveysController";
 import { UserController } from "./controllers/UserController";
@@ -7,6 +9,8 @@ const router = Router();
 const userController = new UserController();
 const surveyController = new SurveysController();
 const sendMailController = new SendMailController();
+const answerController = new AnswerController();
+const npsController = new NpsController();
 
 router.post("/users", userController.create);
 router.get('/users', userController.show);
@@ -15,5 +19,9 @@ router.post("/surveys", surveyController.create);
 router.get("/surveys", surveyController.show);
 
 router.post("/sendMail", sendMailController.execute);
+
+router.get("/answers/:value", answerController.execute);
+
+router.get("/nps/:survey_id", npsController.execute);
 
 export { router };
